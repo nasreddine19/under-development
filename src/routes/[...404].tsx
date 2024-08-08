8dc8ebf4d9eddf5a4e253abcfb1325ca038448a0
NASREDDINE BOUDELLAL <ns.boudellal@gmail.com>
2024-08-08 03:11:58 +0100

    UPD under development notice

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -6,14 +6,7 @@ export default function NotFound() {
     <main>
       <Title>Not Found</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <h1>Page Not Found 😁</h1>
     </main>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,18 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Nasreddine Boudellal</Title>
+      <h1>Welcome to Nasreddine Boudellal's Portfolio</h1>
+      <h4>This site is currently under development.</h4>
+      <p>
+      Thank you for visiting! I'm currently working on creating an amazing portfolio to showcase my work. Please check back soon to see the updates.
+      </p>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        In the meantime, feel free to connect with me on{" "}
+        <a href="https://www.linkedin.com/in/n-boudellal/" target="_blank">
+          LinkedIn.
         </a>{" "}
-        to learn how to build SolidStart apps.
       </p>
     </main>
   );
